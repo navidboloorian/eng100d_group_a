64452e06d8f66602eabc2422aedfbe5cbce31e1c
CM2205 <csmartinez@ucsd.edu>
2023-11-12 12:52:04 -0800

    creating frontend files
    adding header component

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+// import "./HeaderMsg"
+import HeaderMsg from './HeaderMsg';
+
+function App() {
+  return (
+    <HeaderMsg/>
+  );
+}
+
+export default App;
---
+++ frontend/src/HeaderMsg.js
@@ -0,0 +1,15 @@
+import "./HeaderMsg.css"
+
+function HeaderMsg() {
+
+
+    return (
+        <h1 className="HeaderText"> 
+        Welcome to Global Talent Network’s Career Questionnaire. 
+        Take the next 15 minutes to answer the 28 questions to ba matched with potential careers!
+      </h1>
+    );
+
+}
+
+export default HeaderMsg; 
