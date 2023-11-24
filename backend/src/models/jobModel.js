34d65f69835fc429043c8d1adb164a5a5bbd1d8b
Navid Boloorian <navidboloorian0@gmail.com>
2023-11-24 12:25:57 -0800

    deleted backend

--- backend/src/models/jobModel.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const jobSchema = Schema({
-    title: {type: String, required: true},
-    description: {type: String, required: true},
-    link: {type: String, required: true}
-});
-
-export default model("Job", jobSchema);
--- backend/src/models/questionModel.js
+++
@@ -1,7 +0,0 @@
-import { Schema, model } from "mongoose";
-
-const questionSchema = Schema({
-    text: {type: String, required: true},
-});
-
-export default model("Question", questionSchema);
